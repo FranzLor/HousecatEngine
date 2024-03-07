e05a42a996c2f19adccf91ba5f9df94322687ee9
ZeroCalBacon <FranzLor64@yahoo.com>
2024-03-06 23:08:49 -0700

    updated MovementSystem implemented map bound implemented out of map culling on entities

--- HousecatEngine/src/systems/MovementSystem.h
+++ HousecatEngine/src/systems/MovementSystem.h
@@ -20,10 +20,36 @@ public:
 			transform.position.x += static_cast<float>(rigidbody.velocity.x * deltaTime);
 			transform.position.y += static_cast<float>(rigidbody.velocity.y * deltaTime);
 		
-			//TODO collision
-			//stop out of map bounds
-			//use buffer
-			//for nonplayer, kill
+			//prevent player form moving outside map
+			if (entity.HasTag("player")) {
+				//adjust padding for map (-5 on edge)
+				int paddingLeft = -5;
+				int paddingRight = -5;
+				int paddingTop = -5;
+				int paddingBottom = -5;
+
+				paddingLeft *= transform.scale.x;
+				paddingRight *= transform.scale.x;
+				paddingTop *= transform.scale.y;
+				paddingBottom *= transform.scale.y;
+
+				//update position within boundaries
+				transform.position.x = std::max(static_cast<float>(paddingLeft), std::min(transform.position.x, static_cast<float>(Game::mapWidth - paddingRight)));
+				transform.position.y = std::max(static_cast<float>(paddingTop), std::min(transform.position.y, static_cast<float>(Game::mapHeight - paddingBottom)));
+			}
+
+			//checks if outside the map boundaries, buffer margin forgives 200 px W/H
+			int cullingMargin = 200;
+			bool isEntityOutsideMap = (
+				transform.position.x > Game::mapWidth + cullingMargin ||
+				transform.position.x < -cullingMargin ||
+				transform.position.y > Game::mapHeight + cullingMargin ||
+				transform.position.y < -cullingMargin
+				);
+
+			if (!entity.HasTag("player") && isEntityOutsideMap) {
+				entity.Kill();
+			}
 		}
 	}
 
