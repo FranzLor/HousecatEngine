17443d8a6e427c05b1efef650ac5a0825103ab20
ZeroCalBacon <FranzLor64@yahoo.com>
2024-03-09 01:44:07 -0700

    Feature: EditTile mouse interact, edit commands

---
+++ HousecatEngine/src/editor/utilities/EditTile.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "../editmanager/EditManager.h"
+#include "../utilities/mouse/Mouse.h"
+
+#include "../../components/TransformComponent.h"
+#include "../../components/SpriteComponent.h"
+
+class EditAddTile : public IEdit {
+private:
+	int tileID;
+	bool initialStateStored;
+
+	std::shared_ptr<class Mouse> mouse;
+
+	TransformComponent originalTransformComponent;
+	SpriteComponent originalSpriteComponent;
+
+public:
+	EditAddTile(std::shared_ptr<Mouse> mouse);
+	~EditAddTile() = default;
+
+	void Execute() override;
+	void Undo() override;
+	void Redo() override;
+
+};
+
+class EditRemoveTile : public IEdit {
+private:
+	int tileID;
+
+	std::shared_ptr<class Mouse> mouse;
+
+	TransformComponent originalTransformComponent;
+	SpriteComponent originalSpriteComponent;
+
+public:
+	EditRemoveTile(std::shared_ptr<Mouse> mouse);
+	~EditRemoveTile() = default;
+
+	void Execute() override;
+	void Undo() override;
+	void Redo() override;
+
+};
