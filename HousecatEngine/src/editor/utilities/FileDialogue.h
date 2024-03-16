08e596ded0f8f5a874b907feae31f58c8d327bdc
ZeroCalBacon <FranzLor64@yahoo.com>
2024-03-16 04:34:30 -0600

    Updated: FileDialogue parameters for Windows

--- HousecatEngine/src/editor/utilities/FileDialogue.h
+++ HousecatEngine/src/editor/utilities/FileDialogue.h
@@ -10,9 +10,9 @@ public:
 	FileDialogue() = default;
 	~FileDialogue() = default;
 
-	static std::string OpenFile(const char* filterPattern, HWND windowHandle);
+	static std::string OpenFile(const char* filterPattern = "Tilemap Files (*.map) | *.map\0*.map\0", HWND windowHandle = NULL);
 
-	static std::string SaveFile(const char* filterPath, HWND windowHandle);
+	static std::string SaveFile(const char* filterPath = "Tilemap Files (*.map) | *.map\0*.map\0", HWND windowHandle = NULL);
 
 
 
