b2887c30d959a78a45520df8990af1a719ee855c
ZeroCalBacon <FranzLor64@yahoo.com>
2024-03-29 01:42:25 -0600

    Updated: EditorUIRendering -> RenderGrid updated grid line colors and dark mode on boundary. Changed boundary thickness

--- HousecatEngine/src/editor/ui/EditorUIRendering.cpp
+++ HousecatEngine/src/editor/ui/EditorUIRendering.cpp
@@ -255,28 +255,43 @@ void EditorUIRendering::RenderGrid(EditorRenderer& renderer, SDL_Rect& camera, c
 	int xTiles = canvas->GetCanvasWidth() / tileSize;
 	int yTiles = canvas->GetCanvasHeight() / tileSize;
 
-
+	//grid lines
 	if (gridShow) {
-		
-		SDL_SetRenderDrawColor(renderer.get(), 140, 140, 140, SDL_ALPHA_OPAQUE);
+		SDL_SetRenderDrawColor(renderer.get(), 61, 132, 224, SDL_ALPHA_OPAQUE);
 
 		//vertical
-		for (int i = 0; i <= xTiles; i++) {
+		for (int i = 0; i < xTiles; i++) {
 			int x = std::floor(i * tileSize * zoom) - camera.x;
 			SDL_RenderDrawLine(renderer.get(), x, 0 - camera.y, x, (yTiles * tileSize * zoom) - camera.y);
 		}
 
 		//horizontal
-		for (int j = 0; j <= yTiles; j++) {
+		for (int j = 0; j < yTiles; j++) {
 			int y = std::floor(j * tileSize * zoom) - camera.y;
 			SDL_RenderDrawLine(renderer.get(), 0 - camera.x, y, (xTiles * tileSize * zoom) - camera.x, y);
 		}
 	}
 
-	//boundary
-	SDL_SetRenderDrawColor(renderer.get(), 37, 39, 41, SDL_ALPHA_OPAQUE);
+	//boundary styling
+	if (!isDarkMode) {
+		SDL_SetRenderDrawColor(renderer.get(), 37, 39, 41, SDL_ALPHA_OPAQUE);
+	}
+	else {
+		SDL_SetRenderDrawColor(renderer.get(), 218, 216, 214, SDL_ALPHA_OPAQUE);
+	}
+
+	//TODO
+	//imgui button option -> changeable thickness w/ slider
+	//thiccness
+	float boundaryThickness = 1.8f;
+
 	SDL_Rect boundaryRect = { 0 - camera.x, 0 - camera.y, xTiles * tileSize * zoom, yTiles * tileSize * zoom };
-	SDL_RenderDrawRect(renderer.get(), &boundaryRect);
+
+	//expands boundary outside
+	for (float i = 0.0f; i < boundaryThickness; ++i) {
+		SDL_Rect expandedBoundary = { boundaryRect.x - i, boundaryRect.y - i, boundaryRect.w + i * 2.0f, boundaryRect.h + i * 2.0f };
+		SDL_RenderDrawRect(renderer.get(), &expandedBoundary);
+	}
 }
 
 
