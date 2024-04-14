924d3fc497c1b365e0b001a1871a77af0e5d6304
ZeroCalBacon <FranzLor64@yahoo.com>
2024-04-13 23:10:01 -0600

    Updated: Logger -> usage comments

--- HousecatEngine/src/logger/Logger.h
+++ HousecatEngine/src/logger/Logger.h
@@ -6,6 +6,15 @@
 #include <SDL.h>
 #include <imgui/imgui.h>
 
+
+//----------------------------------------------------//
+//                    LOGGER CLASS                    //
+//     Handles logging across different log levels    //
+//   and outputs to console with timestamped entries. //
+//----------------------------------------------------//
+
+
+
 enum LogType {
 	LOG_LIFECYCLE,
 	LOG_DEBUG,
@@ -14,6 +23,7 @@ enum LogType {
 	LOG_ERROR
 };
 
+//individual log messages with their type and message content.
 struct LogMessage {
 	LogType type;
 	std::string message;
@@ -23,22 +33,45 @@ class Logger {
 public:
 	static std::vector<LogMessage> messages;
 
-	//use for constructors / destructors
+	//used for constructors / destructors
 	static void Lifecycle(const std::string& message);
 
-	//use to track | debug
+	//used to track | debug
 	static void Debug(const std::string& message);
 
+	//used for general logging
 	static void Log(const std::string& message);
+
+	//not critical but require attention
 	static void Warning(const std::string& message);
+
+	//critical errors
 	static void Error(const std::string& message);
 
+	//outputs version of SDL being used
 	static void SDLVersion();
 
+	//outputs the version of ImGui being used
 	static void ImGuiVersion();
 
 	Logger() {}
 	~Logger() {}
 
 };
+
+
+
+
+/*
+*
+* GENERAL EXAMPLES:
+* Logger::Log("Entity Created: " + entity.GetID()");
+* 
+* Outputs to console with entity ID.
+* 
+* Logger::Error("Error Loading Texture: " + std::string(SDL_GetError()));
+* 
+* Outputs to console with SDL error message.
+* 
+*/
 
