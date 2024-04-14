30b522a05f0e296ade935ee7e20c39c17412d19d
ZeroCalBacon <FranzLor64@yahoo.com>
2024-04-13 22:11:52 -0600

    Updated: SpriteComponent -> usage comments

--- HousecatEngine/src/components/SpriteComponent.h
+++ HousecatEngine/src/components/SpriteComponent.h
@@ -4,6 +4,14 @@
 #include <glm/glm.hpp>
 #include <string>
 
+//----------------------------------------------------//
+//                     COMPONENT                      //
+//     Manages the visual representation of an        //
+//                 entity within the game.            //
+//----------------------------------------------------//
+
+
+
 struct SpriteComponent {
 	std::string assetID;
 	int width;
@@ -33,3 +41,39 @@ struct SpriteComponent {
 		this->originalColor = color;
 	}
 };
+
+
+
+
+/**
+ *
+ * @struct SpriteComponent
+ * @brief Stores properties necessary for rendering a sprite on the screen.
+ *
+ * This component contains all the important data for rendering a sprite,
+ * including asset ID, dimensions, layer depth (zIndex), and
+ * whether it is a fixed sprite or moves with the camera. It also includes
+ * attributes for dynamically adjusting the sprite's appearance, such as
+ * color modifications and flip orientation.
+ *
+ * Usage:
+ * - assetID: Identifier for the sprite's texture in the AssetManager.
+ * - width, height: Dimensions of the sprite.
+ * - zIndex: The rendering order depth; lower values are rendered first.
+ * - isFixed: If true, the sprite does not move with the camera (useful for UI elements).
+ * - srcRect: The rectangle defining the portion of the texture to draw.
+ * - flip: Defines if the sprite should be flipped horizontally or vertically.
+ * - color, originalColor: Used for dynamic color changes, including highlighting or damage effects.
+ * - colorChangeEndTime: Timestamp after which the sprite's color change effect should revert.
+ *
+ * Example:
+ * 	player.AddComponent<SpriteComponent>("player_texture", 64, 64, 1, false, 0, 0, SDL_FLIP_NONE);
+ * 	
+ *  This sets up the player with a 64x64 sprite, not fixed to the camera, with no initial flip.
+ *
+ * Note:
+ * - The SpriteComponent is primarily used by the RenderSystem to draw entities on the screen.
+ *   It supports sorting entities by zIndex to manage the rendering order and applies visual transformations
+ *   based on the entity's TransformComponent.
+ * 
+ */
