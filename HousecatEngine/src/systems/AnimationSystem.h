baf732916e87281f587ca3822694337466f8580b
ZeroCalBacon <FranzLor64@yahoo.com>
2024-04-14 12:33:51 -0600

    Updated: AnimationSystem -> usage comments

--- HousecatEngine/src/systems/AnimationSystem.h
+++ HousecatEngine/src/systems/AnimationSystem.h
@@ -7,6 +7,12 @@
 #include "../components/SpriteComponent.h"
 
 
+//--------------------------------------------------//
+//                ANIMATION SYSTEM                  //
+// Handles animation logic based on entity movement.//
+//--------------------------------------------------//
+
+
 class AnimationSystem : public System {
 public:
 	AnimationSystem() {
@@ -32,3 +38,38 @@ public:
 		}
 	}
 };
+
+
+
+
+/**
+ *
+ * @class AnimationSystem
+ * @brief Manages the animation states of entities based on their movement states.
+ *
+ * This system updates the animation frames of entities that have AnimationComponent,
+ * SpriteComponent, and MovementStateComponent. It ensures that each entity's sprite
+ * is animated according to its movement, updating frames based on the elapsed time and
+ * the defined frame speed in the AnimationComponent.
+ *
+ * Usage:
+ * - This system must be added to your game project's system manager during initialization.
+ * - It should be updated each frame to reflect the current animations of the entities.
+ *
+ * Example:
+ * 	housecat->AddSystem<AnimationSystem>();
+ * 
+ *  Adding the AnimationSystem in a game project by calling housecat
+ *
+ * 	housecat->GetSystem<AnimationSystem>().Update();
+ * 
+ *  Updating the AnimationSystem in the game loop
+ *
+ * Key Methods:
+ * - Update(): Called on each frame to update the animation frames of all registered entities based
+ *   on their movement states.
+ *
+ * This system is crucial for visually representing the dynamic states of entities in the game,
+ * contributing to a lively and responsive game environment.
+ * 
+ */
