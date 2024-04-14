46c61a64e6ee5b75622cdedbac6e18b4bfc92355
ZeroCalBacon <FranzLor64@yahoo.com>
2024-04-13 23:36:18 -0600

    Updated: Logger -> fixed documentation

--- HousecatEngine/src/logger/Logger.h
+++ HousecatEngine/src/logger/Logger.h
@@ -13,7 +13,16 @@
 //   and outputs to console with timestamped entries. //
 //----------------------------------------------------//
 
-
+/**
+ * @enum LogType
+ * @brief Defines the category of log messages.
+ *
+ * - LOG_LIFECYCLE: Logs related to object lifecycle (creation, destruction).
+ * - LOG_DEBUG: Logs intended for debugging purposes.
+ * - LOG_INFO: Informational logs that provide insights into the application's operation.
+ * - LOG_WARNING: Warnings about potential issues that are not immediately critical.
+ * - LOG_ERROR: Critical error messages that could halt or severely affect operations.
+ */
 
 enum LogType {
 	LOG_LIFECYCLE,
@@ -23,7 +32,14 @@ enum LogType {
 	LOG_ERROR
 };
 
-//individual log messages with their type and message content.
+/**
+ * @struct LogMessage
+ * @brief Represents a single log message with a type and text content.
+ *
+ * This struct encapsulates the data necessary to record a log entry, which includes
+ * the log type and the actual message string.
+ */
+
 struct LogMessage {
 	LogType type;
 	std::string message;
@@ -62,16 +78,24 @@ public:
 
 
 
-/*
-*
-* GENERAL EXAMPLES:
-* Logger::Log("Entity Created: " + entity.GetID()");
-* 
-* Outputs to console with entity ID.
-* 
-* Logger::Error("Error Loading Texture: " + std::string(SDL_GetError()));
-* 
-* Outputs to console with SDL error message.
-* 
-*/
-
+/**
+ *
+ * @class Logger
+ * @brief Provides static methods for logging different types of messages
+ *        with automatic timestamping and type categorization.
+ *
+ * Logger supports logging messages at various levels, from debug to critical errors,
+ * with an interface for adding SDL and ImGui version logging. It centralizes the logging
+ * mechanism to standardize how messages are displayed and stored. This class is intended
+ * to be used across the system for consistent logging practices.
+ *
+ * Usage:
+ *  Logger::Lifecycle("Game Contructor Called!");
+ *  Logger::Debug("Player Position: " + playerPosition.ToString());
+ *  Logger::Log("Key Pressed: " + key);
+ *  Logger::Warning("Entity Table in Lua Script is Empty");
+ *	Logger::Error(Failed to Load SDL Texture: " + SDL_GetError());
+ *
+ * The logger outputs to the console and can store logs for later retrieval or inspection.
+ * 
+ */
