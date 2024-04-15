a3408b8206de5a003baec2b390bb9f16aa218015
ZeroCalBacon <FranzLor64@yahoo.com>
2024-04-14 22:43:05 -0600

    Updated: WinConditionSystem -> usage comments

--- HousecatEngine/src/systems/WinConditionSystem.h
+++ HousecatEngine/src/systems/WinConditionSystem.h
@@ -10,6 +10,13 @@
 #include "../eventmanager/EventManager.h"
 #include "../events/CollisionEvent.h"
 
+
+//----------------------------------------------------//
+//               WIN CONDITION SYSTEM                 //
+// Manages the conditions under which a game is won.  //
+//----------------------------------------------------//
+
+
 class WinConditionSystem : public System {
 private:
     bool winConditionMet = false; 
@@ -61,3 +68,36 @@ public:
 		}
     }
 };
+
+
+
+
+/**
+ *
+ * @class WinConditionSystem
+ * @brief Manages game win conditions by monitoring collisions that fulfill winning criteria.
+ *
+ * This system uses the ECS framework to check interactions between entities that might trigger a win scenario.
+ * Entities involved in win conditions must have both a WinConditionComponent and a BoxColliderComponent.
+ * The system listens for collision events that might signal a game-winning condition.
+ *
+ * Usage:
+ * - The system must be added to the game engine setup with a reference to the main game object for triggering game state changes.
+ * - It subscribes to collision events and evaluates if these collisions meet the defined win conditions.
+ *
+ * Example:
+ * 	housecat->AddSystem<WinConditionSystem>(*this);
+ * 
+ *  Adding the WinConditionSystem to the game using Housecat
+ *
+ * 	housecat->GetSystem<WinConditionSystem>().ListenToEvents(eventManager);
+ * 
+ *  Setup listening for events in the game loop
+ *
+ * Key Methods:
+ * - ListenToEvents(EventManager&): Initializes the system to respond to relevant game events.
+ * - HandleCollision(CollisionEvent&): Checks if the collision meets win conditions and handles the game state accordingly.
+ *
+ * This system ensures that winning the game is a result of specific entity interactions, enhancing gameplay dynamics and player satisfaction.
+ * 
+ */
